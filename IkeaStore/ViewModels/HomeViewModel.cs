577d176c0c3e4dde42837a91eb42c69f51253ba8
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-03-15 18:30:11 +0100

    Open social apps depending on clicked icon in home page footer

--- IkeaStore/ViewModels/HomeViewModel.cs
+++ IkeaStore/ViewModels/HomeViewModel.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace IkeaStore.ViewModels
 {
     public class HomeViewModel : INotifyPropertyChanged
     {
+        public ICommand OpenFacebookPage { get; private set; }
+        public ICommand OpenYoutubePage { get; private set; }
+        public ICommand OpenTwitterPage { get; private set; }
+
+        private Uri uri;
+
         public HomeViewModel()
         {
+            OpenFacebookPage = new Command(OpenFacebook);
+            OpenYoutubePage = new Command(OpenYoutube);
+            OpenTwitterPage = new Command(OpenTwitter);
         }
 
         #region: Header view
-
             // Search list logic
             private bool isSearchListActive;
 
@@ -54,6 +65,77 @@ namespace IkeaStore.ViewModels
                         }
                     }
             }
+
+        #endregion
+
+        #region: Footer view
+        //Open the specified app if supported otherwise open the browser.
+
+        public async void OpenFacebook()
+        {
+            try
+            {
+                uri = new Uri("fb://IKEAdeutschland");
+                bool isAppInstalledOnDevice = await Launcher.CanOpenAsync(uri);
+
+                if (isAppInstalledOnDevice)
+                {
+                    await Launcher.OpenAsync(uri);
+                }
+                else
+                {
+                    uri = new Uri("https://www.facebook.com/IKEAdeutschland/?brand_redir=DISABLE");
+                    await Launcher.OpenAsync(uri);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        public async void OpenYoutube()
+        {
+            try
+            {
+                uri = new Uri("vnd.youtube://IKEAdeutschland");
+                bool isAppInstalledOnDevice = await Launcher.CanOpenAsync(uri);
+
+                if (isAppInstalledOnDevice)
+                {
+                    await Launcher.OpenAsync(uri);
+                }
+                else
+                {
+                    uri = new Uri("https://m.youtube.com/IKEAdeutschland/");
+                    await Launcher.OpenAsync(uri);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        public async void OpenTwitter()
+        {
+            try
+            {
+                uri = new Uri("twitter://IKEAdeutschland");
+                bool isAppInstalledOnDevice = await Launcher.CanOpenAsync(uri);
+
+                if (isAppInstalledOnDevice)
+                {
+                    await Launcher.OpenAsync(uri);
+                }
+                else
+                {
+                    uri = new Uri("https://twitter.com/IKEA_Presse");
+                    await Launcher.OpenAsync(uri);
+                }
+            }
+            catch (Exception e)
+            {
+            }
+        }
 
         #endregion
 
