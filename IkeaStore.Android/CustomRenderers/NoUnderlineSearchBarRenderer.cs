75a950c14c8707b8cfaaf2b835b1bf27b65f9e65
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-03-15 01:14:25 +0100

    Remove search bar underline

---
+++ IkeaStore.Android/CustomRenderers/NoUnderlineSearchBarRenderer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.Widget;
+using IkeaStore.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:ExportRenderer(typeof(SearchBar), typeof(NoUnderlineSearchBarRenderer))]
+namespace IkeaStore.Droid.CustomRenderers
+{
+    public class NoUnderlineSearchBarRenderer : SearchBarRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                LinearLayout linearLayout = this.Control.GetChildAt(0) as LinearLayout;
+                linearLayout = linearLayout.GetChildAt(2) as LinearLayout;
+                linearLayout = linearLayout.GetChildAt(1) as LinearLayout;
+
+                linearLayout.Background = null; //removes underline
+            }
+        }
+    }
+}
