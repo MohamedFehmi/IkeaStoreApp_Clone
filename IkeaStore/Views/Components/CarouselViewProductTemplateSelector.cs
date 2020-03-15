ee4fd14cb45d5849b66b84a39eed819c24a98703
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-03-15 22:43:12 +0100

    Added two content views as the templates of the carousel view in tabbed view

---
+++ IkeaStore/Views/Components/CarouselViewProductTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace IkeaStore.Views.Components
+{
+    public class CarouselViewProductTemplateSelector : DataTemplateSelector
+    {
+
+
+        public CarouselViewProductTemplateSelector()
+        {
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
